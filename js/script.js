d8233ff1856e23b48ead38895a62dbbe7e68c469
Steveh77 <stederosa76@gmail.com>
2022-07-14 18:39:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Milestone 1 (DAY 1)
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+
+
+
+console.log("vue ok", Vue)
+
+const root = new Vue({
+    el:'#root',
+    data:{
+        user: {
+            name: 'Stefano',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
+    }
+    
+})
